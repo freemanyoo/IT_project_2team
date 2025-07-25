47f165902bd245c735133e6d05bc212f6a91e90c
coko9437 <coko9437@gmail.com>
2025-07-25 15:58:36 +0900

    250725_GIT

---
+++ src/test/java/webproject_2team/lunch_matching/LunchMatchingApplicationTests.java
@@ -0,0 +1,13 @@
+package webproject_2team.lunch_matching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunchMatchingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
